14b35875ab93160b8e112ed65b37b0bd5b6eacda
nyxsraid <sahrulramdan.75@gmail.com>
2023-01-17 18:06:30 +0700

    Adding new Preview

--- src/pages/preview/preview.jsx
+++ src/pages/preview/preview.jsx
@@ -6,20 +6,22 @@ import { Container } from "../../components/builder/items/container";
 import { Card } from "../../components/builder/items/card";
 import { CardTop } from "../../components/builder/items/card";
 import { CardBottom } from "../../components/builder/items/card";
+import { Image } from "../../components/builder/items/image";
 import Navbar from "../../components/navbar/navbar";
 import LZUTF8 from "lzutf8";
 import StateLoader from "./stateLoader/stateLoader";
+import { Viewport } from "../../components/builder/base/viewport";
 
 function Preview() {
   return (
     <>
       <Navbar preview={true} />
-      <Editor resolver={{ Text, Button, Container, Card, CardTop, CardBottom }}>
+      <Editor resolver={{ Text, Button, Container, Card, CardTop, CardBottom, Image,Viewport }}>
         <StateLoader />
         <div className="flex justify-center">
           <Frame>
             <Element
-              is={Container}
+              is={Viewport}
               width={30}
               padding={20}
               background="#eee"
